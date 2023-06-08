55227f7f81ceba1256e892803dad54674a909981
Alanjiang712 <50577160+Alanjiang712@users.noreply.github.com>
2023-06-09 00:03:52 +0800

    Delete Python/CC1_FinalProject/CC1_FinalProject directory

--- Python/CC1_FinalProject/CC1_FinalProject/classes.py
+++
@@ -1,6 +0,0 @@
-class Product:
-    def __init__(self, productName, productPrice, productQuantity):
-        self.productName = productName
-        self.productPrice = productPrice
-        self.productQuantity = productQuantity
-
