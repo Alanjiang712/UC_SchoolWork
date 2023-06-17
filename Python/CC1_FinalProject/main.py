8df61b811c65f6f27031b8a59891474764a1a749
Alanjiang712 <50577160+Alanjiang712@users.noreply.github.com>
2023-06-17 19:27:44 +0800

    Add files via upload

--- Python/CC1_FinalProject/main.py
+++ Python/CC1_FinalProject/main.py
@@ -1,8 +1,9 @@
-# Please check my full code in this github repository: https://github.com/Alanjiang712/UC_SchoolWork/tree/main
-# I don't know how to submit a full folder in canvas
+# https://github.com/Alanjiang712/UC_SchoolWork/tree/main/Python/CC1_FinalProject
 
 from classes import Product
-from productsList import productsList
+from datetime import datetime, timedelta
+
+productsList = []
 
 def add_product():
     productName = input('Please enter the name of the new product: ')
@@ -42,6 +43,72 @@ def update_product():
     else:
         print("Product not found")
 
+def generate_sales_report():
+    reportType = input('Enter "1" for daily sales report\nEnter "2" for weekly sales report\nEnter "3" for monthly sales report\nPlease enter your choice: ')
+
+    if reportType == "1":
+        # Generate daily sales report
+        print("Generating daily sales report...")
+        report_date = datetime.now().date()
+        total_sales = 0
+        for product in productsList:
+            total_sales += float(product.productPrice) * int(product.productQuantity)
+        print(f"Daily Sales Report - {report_date}:")
+        print(f"Total Sales: {total_sales}")
+
+    elif reportType == "2":
+        # Generate weekly sales report
+        print("Generating weekly sales report...")
+        start_date = datetime.now().date() - timedelta(days=7)
+        end_date = datetime.now().date()
+        total_sales = 0
+        for product in productsList:
+            total_sales += float(product.productPrice) * int(product.productQuantity)
+        print(f"Weekly Sales Report - {start_date} to {end_date}:")
+        print(f"Total Sales: {total_sales}")
+
+    elif reportType == "3":
+        # Generate monthly sales report
+        print("Generating monthly sales report...")
+        today = datetime.now().date()
+        start_date = datetime(today.year, today.month, 1).date()
+        end_date = start_date + timedelta(days=30)
+        total_sales = 0
+        for product in productsList:
+            total_sales += float(product.productPrice) * int(product.productQuantity)
+        print(f"Monthly Sales Report - {start_date} to {end_date}:")
+        print(f"Total Sales: {total_sales}")
+
+    else:
+        print('Invalid choice. Please try again')
+
+def create_order():
+    order = []
+    while True:
+        productName = input('Enter the name of the product to add to the order (or "done" to finish): ')
+        if productName == 'done':
+            break
+        
+        product = next((p for p in productsList if p.productName == productName), None)
+        if product:
+            quantity = int(input('Enter the quantity: '))
+            if quantity <= int(product.productQuantity):
+                order.append((product, quantity))
+                product.productQuantity = str(int(product.productQuantity) - quantity)
+                print('Product added to order successfully')
+            else:
+                print('Insufficient quantity. Please enter a valid quantity.')
+        else:
+            print('Product not found. Please enter a valid product name.')
+    
+    print('Order Summary:')
+    total_cost = 0
+    for product, quantity in order:
+        cost = float(product.productPrice) * quantity
+        total_cost += cost
+        print(f'Product: {product.productName} - Quantity: {quantity} - Cost: {cost}')
+    print(f'Total Order Cost: {total_cost}')
+
 try:
     print('Welcome to the online store management system')
 
@@ -66,15 +133,20 @@ try:
                 update_product()
             else:
                 print('Invalid choice. Please try again')
+
         elif mainMenuSelection == "2":
-            print('Create Order')
+            create_order()
+
         elif mainMenuSelection == "3":
-            print('Generate Sales Reports')
+            generate_sales_report()
+
         elif mainMenuSelection == "4":
             print('Display Top-Selling Products')
+
         elif mainMenuSelection == "5":
             print('Exiting...')
             break
+
         else:
             print('Invalid choice. Please try again')
 
